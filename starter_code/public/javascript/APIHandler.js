36b2b7137f35fd630135419a114db32bfeadd470
Ravi Kalia <ravkalia@gmail.com>
2018-02-20 17:08:14 -0600

    iteration 1 and 2

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,49 @@
+const axios = require('axios')
+
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = baseUrl
+    this.axios = axios()
+    
   }
 
   getFullList () {
-
+    this.axios.get(this.BASE_URL)
+      .then(
+        renderFetchAll(response)
+      )
+      .catch()
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    this.axios.get(`${this.BASE_URL}/${id}`)
+      .then(
+        renderFetchOne(response)
+      )
+      .catch()
   }
 
   createOneRegister () {
-
+    this.axios.post(this.BASE_URL)
+      .then(
+        renderNewCharacterForm(response)
+      )
+      .catch()
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id) {
+    this.axios.put(`${this.BASE_URL}/${id}`)
+      .then(
+        renderEditCharacterForm()
+      )
+      .catch()
   }
 
   deleteOneRegister () {
-
+    this.axios.delete(`${this.BASE_URL}/${id}`)
+      .then(
+        renderDeleteOne()
+      )
+      .catch()
   }
 }
